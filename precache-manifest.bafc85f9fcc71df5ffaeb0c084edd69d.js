0e5104be0cafaaf16794361eb8e74b725d69f807
Luna <1017929760@qq.com>
2020-07-29 18:37:27 +0800

    deploy

---
+++ precache-manifest.bafc85f9fcc71df5ffaeb0c084edd69d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d62b2ad2787ad84280ae17ebd2592ac",
+    "url": "/dailycost-react-website/index.html"
+  },
+  {
+    "revision": "56d86c44ff7e7e63e026",
+    "url": "/dailycost-react-website/static/css/main.8bf42e60.chunk.css"
+  },
+  {
+    "revision": "2f3adcc399737f734e04",
+    "url": "/dailycost-react-website/static/js/2.ce1ef68a.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/dailycost-react-website/static/js/2.ce1ef68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d86c44ff7e7e63e026",
+    "url": "/dailycost-react-website/static/js/main.90314cef.chunk.js"
+  },
+  {
+    "revision": "ffa5fc2327c67a855476",
+    "url": "/dailycost-react-website/static/js/runtime-main.dbc82ace.js"
+  }
+]);
